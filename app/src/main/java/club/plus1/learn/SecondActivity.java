ee8b8630faf4069a9b616d15c2e7d1a7c3d24b11
Lavsexpert <lavrov-sergey@yandex.ru>
2019-02-02 21:58:51 +0300

    Изменено: запуск поиска в гугле при нажатии кнопки в SecondActivity
    При нажатии на кнопку в SecondActivity теперь открывается браузер
    с поиском в Гугле, в который вбит текст, переданный из editText.

--- app/src/main/java/club/plus1/learn/SecondActivity.java
+++ app/src/main/java/club/plus1/learn/SecondActivity.java
@@ -1,13 +1,27 @@
 package club.plus1.learn;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
     public static String KEY = "KEY";
     private TextView textView;
+    private Button buttonSearch;
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://www.google.com/search?q="+textView.getText()));
+            startActivity(browserIntent);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +33,8 @@ public class SecondActivity extends AppCompatActivity {
         if(bundle != null){
             textView.setText(bundle.getString(KEY));
         }
+
+        buttonSearch = findViewById(R.id.buttonSearch);
+        buttonSearch.setOnClickListener(onClickListener);
     }
 }
