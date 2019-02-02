838931f80d2cec882eb8075c23c6c16f55f2c74c
Lavsexpert <lavrov-sergey@yandex.ru>
2019-02-02 17:55:56 +0300

    Добавлены button и editText
    При нажатии на button появляется Toast с текстом из editText.

--- app/src/main/java/club/plus1/learn/MainActivity.java
+++ app/src/main/java/club/plus1/learn/MainActivity.java
@@ -2,6 +2,9 @@ package club.plus1.learn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        EditText editText = this.findViewById(R.id.editText);
+        String text = editText.getText().toString();
+        if(!text.isEmpty()) {
+            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+        }
     }
 }
